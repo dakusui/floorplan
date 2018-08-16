e9ec42044719822f0e1d0c5141d649869d0a3ccc
Hiroshi Ukai <dakusui@users.noreply.github.com>
2018-08-17 07:38:49 +0900

    Master fix test failures (#32)
    * Fix test failure.
    * Convert to 'interface style'
    * Bump up version to 4.0.0.

--- src/test/java/com/github/dakusui/floorplan/ut/tdesc/AttributeTest.java
+++ src/test/java/com/github/dakusui/floorplan/ut/tdesc/AttributeTest.java
@@ -2,21 +2,30 @@ package com.github.dakusui.floorplan.ut.tdesc;
 
 import com.github.dakusui.floorplan.exception.Exceptions;
 import com.github.dakusui.floorplan.ut.components.SimpleComponent;
+import com.github.dakusui.floorplan.ut.utils.UtBase;
 import org.junit.Test;
 
-import static com.github.dakusui.crest.Crest.allOf;
-import static com.github.dakusui.crest.Crest.asString;
-import static com.github.dakusui.crest.Crest.assertThat;
+import static com.github.dakusui.crest.Crest.*;
 
-public class AttributeTest {
+public class AttributeTest extends UtBase {
   @Test
   public void testInconsistentMessageSupplier() {
+    System.out.println(SimpleComponent.INSTANCE_NAME.name());
+    System.out.println(UtComponent.NAME.name());
     assertThat(
-        Exceptions.inconsistentSpecMessageSupplier(SimpleComponent.INSTANCE_NAME, UtComponent.Attr.NAME).get(),
+        Exceptions.inconsistentSpecMessageSupplier(SimpleComponent.INSTANCE_NAME, UtComponent.NAME).get(),
         allOf(
             asString().containsString(SimpleComponent.INSTANCE_NAME.name()).$(),
-            asString().containsString(UtComponent.Attr.NAME.name()).$()
+            asString().containsString(UtComponent.NAME.name()).$()
         )
+    );
+  }
+
+  @Test
+  public void testAttributeName() {
+    assertThat(
+        SimpleComponent.INSTANCE_NAME.name(),
+        asString().equalTo("INSTANCE_NAME").$()
     );
   }
 }
--- src/test/java/com/github/dakusui/floorplan/ut/tdesc/UtComponent.java
+++ src/test/java/com/github/dakusui/floorplan/ut/tdesc/UtComponent.java
@@ -4,22 +4,8 @@ import com.github.dakusui.floorplan.component.Attribute;
 import com.github.dakusui.floorplan.component.ComponentSpec;
 import com.github.dakusui.floorplan.resolver.Resolvers;
 
-public class UtComponent {
-  public enum Attr implements Attribute {
-    NAME(SPEC.property(String.class).defaultsTo(Resolvers.immediate("helloUtComponent")).$());
+public interface UtComponent extends Attribute {
+  ComponentSpec<UtComponent> SPEC = new ComponentSpec.Builder<>(UtComponent.class).build();
 
-    final private Bean<Attr> bean;
-
-    Attr(Bean<Attr> bean) {
-      this.bean = bean;
-    }
-
-    @SuppressWarnings("unchecked")
-    @Override
-    public Bean<Attr> bean() {
-      return this.bean;
-    }
-  }
-
-  public static final ComponentSpec<Attr> SPEC = new ComponentSpec.Builder<>(Attr.class).build();
+  UtComponent NAME = Attribute.create(SPEC.property(String.class).defaultsTo(Resolvers.immediate("helloUtComponent")).$());
 }
