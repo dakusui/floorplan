dcd245ee6858ae83d1692c372ff5fc7628431cd3
Hiroshi Ukai <dakusui@gmail.com>
2020-02-16 14:34:12 +0900

    Use ObjectSynthesizer provided by osynth library.

--- src/main/java/com/github/dakusui/floorplan/utils/ObjectSynthesizer.java
+++ src/main/java/com/github/dakusui/floorplan/utils/ObjectSynthesizer.java
@@ -1,16 +1,22 @@
 package com.github.dakusui.floorplan.utils;
 
-import com.github.dakusui.objsynth.MethodHandler;
+import com.github.dakusui.osynth.MethodHandler;
 
 import java.lang.reflect.Method;
-import java.util.List;
 import java.util.function.Predicate;
 
+/**
+ * A class for backward compatibility.
+ * Use {@code com.github.dakusui.osynth.ObjectSynthesizer} directly.
+ * @param <T> A type of object to be synthesized.
+ * @deprecated use
+ */
 @Deprecated
-public class ObjectSynthesizer<T> extends com.github.dakusui.objsynth.ObjectSynthesizer.ObjectFactory<T> {
+public class ObjectSynthesizer<T> {
+  private final Builder<T> builder;
 
-  protected ObjectSynthesizer(Class<T> anInterface, List<? extends MethodHandler> handlers, Object fallbackObject) {
-    super(anInterface, handlers, fallbackObject);
+  ObjectSynthesizer(Builder<T> builder) {
+    this.builder = builder;
   }
 
   public static <T> ObjectSynthesizer.Builder<T> builder(Class<T> anInterface) {
@@ -18,18 +24,14 @@ public class ObjectSynthesizer<T> extends com.github.dakusui.objsynth.ObjectSynt
   }
 
   public static MethodHandler.Builder methodCall(String methodName, Class<?>... parameterTypes) {
-    return com.github.dakusui.objsynth.ObjectSynthesizer.methodCall(methodName, parameterTypes);
+    return com.github.dakusui.osynth.ObjectSynthesizer.methodCall(methodName, parameterTypes);
   }
 
-  public interface Handler extends MethodHandler, Predicate<Method> {
-    class Builder extends MethodHandler.Builder {
-      public Builder(Predicate<Method> matcher) {
-        super(matcher);
-      }
-    }
+  public T synthesize() {
+    return this.builder.synthesize();
   }
 
-  public static class Builder<T> extends com.github.dakusui.objsynth.ObjectSynthesizer<T> {
+  public static class Builder<T> extends com.github.dakusui.osynth.SimpleObjectSynthesizer<T> {
     public Builder(Class<T> anInterface) {
       super(anInterface);
     }
@@ -44,9 +46,16 @@ public class ObjectSynthesizer<T> extends com.github.dakusui.objsynth.ObjectSynt
       return (Builder<T>) super.fallbackTo(fallbackObject);
     }
 
-    @Override
-    public ObjectFactory<T> build() {
-      return super.build();
+    public ObjectSynthesizer<T> build() {
+      return new ObjectSynthesizer<>(this);
+    }
+  }
+
+  public interface Handler extends MethodHandler, Predicate<Method> {
+    class Builder extends MethodHandler.Builder {
+      public Builder(Predicate<Method> matcher) {
+        super(matcher);
+      }
     }
   }
 }
